b462345297283c9622c211271d509e89eb3c98f5
Manuel Dávila <43458297+maadcode@users.noreply.github.com>
2025-01-22 00:45:24 +0000

    First web api

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace NetworkingCapital.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
